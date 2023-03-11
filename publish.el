41d845d718ac8eacce1d934669b6a30183b65d4b
Charl P. Botha <cpbotha@vxlabs.com>
2023-03-11 11:10:57 +0200

    Add more comments

--- publish.el
+++ publish.el
@@ -8,10 +8,15 @@
              (message "=====> org-link-search IGNORED ERROR: %s" err)
              ""))))
 
+;; if you DO WANT ox-hugo to error out on each missing link so you can fix it, just comment out the line below
 (advice-add #'org-hugo--search-and-get-anchor :around #'oh--saga)
 
 (recentf-mode -1)
 
+;; this is the main publish org -> md function
+;; it improves over jethrokuan's original in the following ways:
+;; - supports nested org-files (it will maintain the directory structure on the hugo side)
+;; - overrides any HUGO directives you might already have in your org mode files
 (defun cpb/publish (org-dir in_file hugo-base-dir out_file)
   (with-current-buffer (find-file-noselect in_file)
     ;; unfortunately, these are overridden by e.g. #+HUGO_BASE_DIR in the file if they are present
@@ -21,9 +26,7 @@
            (org-hugo-content (file-name-concat org-hugo-base-dir "content"))
            ;; the section is the directory relative to "content" containing the output md
            (org-hugo-section (file-name-directory (file-relative-name out_file org-hugo-content)))
-           (cpb-oh-pub-dir (file-name-concat org-hugo-content org-hugo-section))
-           
-           )
+           (cpb-oh-pub-dir (file-name-concat org-hugo-content org-hugo-section)))
 
       ;; org-hugo--get-pub-dir is called by org-hugo-export-to-md right after reading all hugo vars
       ;; we override it in order to
